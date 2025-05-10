cf19f6f92abe2fede15cdcdd8a03cf13e53de1a7
Amna shakil <hafizaaminarehman@gmail.com>
2025-05-10 05:33:37 +0500

    Add files via upload

---
+++ data_types_prictices.py
@@ -0,0 +1,33 @@
+#Integer Example
+age=16
+score=90
+x=-10
+y=3
+print("Age:", age)
+print("Score:", score)
+print("X:", x)
+print("Y:", y)
+#String Example
+name="shakeel"
+message="Hello! I am zara."
+message_01= "university of lahore"
+message_02= "where are you from?"
+print("Name:",name)
+print("Message:", message)
+print("Message_01:", message_01)
+print("Message_02:", message_02)
+#Flaot Example
+price= 25.5
+temperature= 17.5 #Celcius7
+milk_quantity= 1.5 # Liter
+print("Price:"price)
+print("temperature:"temperature)
+print("Milk_Quantity:"milk_quantity)
+#Boolean Example
+light_on= "true"
+light_off="false"
+print("True:", true)
+print("False:",false)
+
+
+
